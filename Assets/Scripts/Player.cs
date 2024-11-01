700572bb2fd04b95cbdf5ca6008db863ee30dcc2
Andhika Fadhlan Wijanarko <dhika@MacBook-Air-Andhika.local>
2024-11-01 23:47:42 +0700

    CS7-Unity: Galaxy Shooter

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class NewMonoBehaviourScript : MonoBehaviour {
+
+    private PlayerMovement playerMovement
+    private Animator animator
+    void awake()
+    {
+
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+
+    }
+}
